236ac57d37802fff2501fa7fb301b826370ae11d
excelion29 <wronkowcanolt@gmail.com>
2022-10-21 14:26:27 -0500

    cambios 14:26

--- Bioelectron2022RayosX/Functions/Services/RayosXTomografia/tomografia_uniformidad_espacial_numero_ct.py
+++ Bioelectron2022RayosX/Functions/Services/RayosXTomografia/tomografia_uniformidad_espacial_numero_ct.py
@@ -5,15 +5,17 @@ from Functions.Services.valor_minimo import valor_minimo
 def tomografia_uniformidad_espacial_numero_ct(element, attribute):
     resultado = [{"parametros":"","resultado":0,"condicion":True}]
     Uc = []
+    tolerancia = True
+
     for x in attribute:
         operacion = float(element[0])-float(x)
         Uc.append(operacion)
-    
+        
     if(valor_absoluto(valor_maximo(Uc))>=valor_absoluto(valor_minimo(Uc))):
         operacion_2 = valor_maximo(Uc)
     else:
         operacion_2 = valor_minimo(Uc)
-
+        
     redondear = round(operacion_2,2)
 
     if(redondear <= 5):
@@ -21,13 +23,7 @@ def tomografia_uniformidad_espacial_numero_ct(element, attribute):
     else:
         tolerancia = False
         
-    return resultado = [
-        {
-            "parametros":"",
-            "resultado":redondear,
-            "condicion":tolerancia
-        }
-    ]
+    return resultado = [{"parametros":"","resultado":redondear,"condicion":tolerancia}]
     
 
 
