351906afd7775d11121e83d42b93a902d62a92ed
excelion29 <wronkowcanolt@gmail.com>
2022-11-08 13:00:14 -0500

    data

--- Bioelectron2022RayosX/User/models.py
+++ Bioelectron2022RayosX/User/models.py
@@ -32,7 +32,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     last_name = models.CharField('Apellidos', max_length = 255, blank = True, null = True)
     image = models.ImageField('Imagen de perfil', upload_to='perfil/', max_length=255, null=True, blank = True)
     is_active = models.BooleanField(default = True)
-    is_staff = models.BooleanField(default = False)
+    is_staff = models.BooleanField(default = True)
     historical = HistoricalRecords()
     objects = UserManager()
 
