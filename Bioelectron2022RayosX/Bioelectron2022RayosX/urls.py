2eb9917a3c67b256a59034a71499b6a6395b765f
excelion29 <wronkowcanolt@gmail.com>
2022-11-08 11:45:53 -0500

    cambios en urls

--- Bioelectron2022RayosX/Bioelectron2022RayosX/urls.py
+++ Bioelectron2022RayosX/Bioelectron2022RayosX/urls.py
@@ -57,8 +57,7 @@ urlpatterns = [
     
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
-
+    path('', admin.site.urls),
     path('api/', include('APIS.urls')),
 ]
 
