0994c8ec9fc978bfe67838e18c05449d59fdcfe0
excelion29 <wronkowcanolt@gmail.com>
2022-11-08 12:13:34 -0500

    url change

--- Bioelectron2022RayosX/Bioelectron2022RayosX/urls.py
+++ Bioelectron2022RayosX/Bioelectron2022RayosX/urls.py
@@ -43,22 +43,27 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
+    
+    path('api/', include('APIS.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
-
     path('admin/', admin.site.urls),
+    path('', admin.site.urls),
+    
+
+
+
 
 
     # path('', views.index),
     # path('report_create/', views.report_create),
     # path('login/', views.login),
     
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('', admin.site.urls),
-    path('api/', include('APIS.urls')),
 ]
 
 urlpatterns += [
